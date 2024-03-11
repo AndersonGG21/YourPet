3b88bcef71c9c941fac8604f514366d536158185
AndersonGG21 <andersongarcesgarcia@gmail.com>
2024-03-10 22:45:31 -0500

    Refactor home component and app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'home',
-     loadComponent: ()=> import('./pages/home/home.component') 
+    { 
+			path: 'home',
+			loadComponent: ()=> import('./pages/home/home.component') 
     },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full'}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export default class HomeComponent {
+export default class HomeComponent implements OnInit{
+  
+  ngOnInit(): void {
+    const listItem = document.querySelectorAll('.home-header li');
+    const menuBackdrop = document.querySelector('#menu-backdrop') as HTMLDivElement;
 
+    listItem.forEach((item) => {
+      item.addEventListener('mouseenter', ({ target} : any) => {
+        const { left, top, width, height } = item.getBoundingClientRect();
+        menuBackdrop.style.setProperty("--left", `${left}px`);
+        menuBackdrop.style.setProperty("--top", `${top}px`);
+        menuBackdrop.style.setProperty("--width", `${width}px`);
+        menuBackdrop.style.setProperty("--heigth", `${height}px`);
+        menuBackdrop.style.visibility = 'visible';
+        menuBackdrop.style.opacity = '1';
+      })
+
+      item.addEventListener('mouseleave', () => {
+        menuBackdrop.style.visibility = 'hidden';
+        menuBackdrop.style.opacity = '0';
+      })
+    })
+  }
+
+  
 }
