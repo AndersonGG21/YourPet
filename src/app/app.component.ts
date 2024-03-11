4719470e248f70f5f8e5d74246ced3de2f2455b6
AndersonGG21 <andersongarcesgarcia@gmail.com>
2024-03-10 20:58:14 -0500

    Update app routes and configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AccordionModule } from 'primeng/accordion';
+import { PrimeNGConfig } from 'primeng/api';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, AccordionModule, RippleModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'yourpet';
+export class AppComponent implements OnInit{
+  title = 'YourPet App';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
