41182026f1e6bc57870e02519b4006013b8bb7de
AndersonGG21 <andersongarcesgarcia@gmail.com>
2024-03-13 18:35:49 -0500

    Remove header and menu backdrop from home component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent implements OnInit{
+
+  ngOnInit(): void {
+    const listItem = document.querySelectorAll('.home-header li');
+    const menuBackdrop = document.querySelector('#menu-backdrop') as HTMLDivElement;
+
+    listItem.forEach((item) => {
+      item.addEventListener('mouseenter', ({ target} : any) => {
+        const { left, top, width, height } = item.getBoundingClientRect();
+        menuBackdrop.style.setProperty("--left", `${left}px`);
+        menuBackdrop.style.setProperty("--top", `${top}px`);
+        menuBackdrop.style.setProperty("--width", `${width}px`);
+        menuBackdrop.style.setProperty("--heigth", `${height}px`);
+        menuBackdrop.style.visibility = 'visible';
+        menuBackdrop.style.opacity = '1';
+      })
+
+      item.addEventListener('mouseleave', () => {
+        menuBackdrop.style.visibility = 'hidden';
+        menuBackdrop.style.opacity = '0';
+      })
+    })
+  }
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,25 +11,7 @@ import { NgOptimizedImage } from '@angular/common'
 export default class HomeComponent implements OnInit{
   
   ngOnInit(): void {
-    const listItem = document.querySelectorAll('.home-header li');
-    const menuBackdrop = document.querySelector('#menu-backdrop') as HTMLDivElement;
-
-    listItem.forEach((item) => {
-      item.addEventListener('mouseenter', ({ target} : any) => {
-        const { left, top, width, height } = item.getBoundingClientRect();
-        menuBackdrop.style.setProperty("--left", `${left}px`);
-        menuBackdrop.style.setProperty("--top", `${top}px`);
-        menuBackdrop.style.setProperty("--width", `${width}px`);
-        menuBackdrop.style.setProperty("--heigth", `${height}px`);
-        menuBackdrop.style.visibility = 'visible';
-        menuBackdrop.style.opacity = '1';
-      })
-
-      item.addEventListener('mouseleave', () => {
-        menuBackdrop.style.visibility = 'hidden';
-        menuBackdrop.style.opacity = '0';
-      })
-    })
+    
   }
 
   
